9c80440b0790ddfe0b8b4858d4dc719ece5258f1
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-01-28 15:16:03 -0600

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by student on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
